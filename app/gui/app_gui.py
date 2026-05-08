600ee9ada9853b7459baa3e20ca258956fd919f4
yang12-1 <a01050485358@gmail.com>
2026-05-08 20:29:42 +0900

    Create app_gui.py

---
+++ app/gui/app_gui.py
@@ -0,0 +1,24 @@
+import customtkinter as ctk
+
+app = ctk.CTk()
+
+app.geometry("1000x700")
+
+app.title("VTuber Viral Editor AI")
+
+label = ctk.CTkLabel(
+    app,
+    text="VTuber Viral Editing System",
+    font=("Arial", 30)
+)
+
+label.pack(pady=30)
+
+button = ctk.CTkButton(
+    app,
+    text="Start Editing"
+)
+
+button.pack(pady=20)
+
+app.mainloop()
