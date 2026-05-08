be62f3948fba0a30886747971cf3331046458286
yang12-1 <a01050485358@gmail.com>
2026-05-08 21:00:56 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
+from app.downloader.youtube_downloader import download_youtube_video
+
 from app.analyzer.scene_detector import detect_scenes
 from app.analyzer.whisper_transcriber import transcribe_video
+
 from app.editor.auto_cut import create_highlight
 from app.editor.subtitle_renderer import add_subtitles
 from app.editor.zoom_effect import apply_zoom_effect
 from app.editor.shorts_formatter import convert_to_vertical
 from app.editor.ffmpeg_renderer import render_shorts
 
-VIDEO_PATH = "input/video.mp4"
+
+# YouTube 링크 입력
+YOUTUBE_URL = input("YouTube URL: ")
+
+print("Downloading YouTube Video...")
+VIDEO_PATH = download_youtube_video(YOUTUBE_URL)
+
+print("Download Complete")
+
 
 print("Scene Detection...")
 scenes = detect_scenes(VIDEO_PATH)
@@ -29,8 +40,11 @@ clip = add_subtitles(clip, subtitles)
 TEMP_OUTPUT = "output/temp.mp4"
 FINAL_OUTPUT = "output/final_shorts.mp4"
 
-
-clip.write_videofile(TEMP_OUTPUT)
+clip.write_videofile(
+    TEMP_OUTPUT,
+    codec="libx264",
+    audio_codec="aac"
+)
 
 print("FFmpeg Rendering...")
 render_shorts(TEMP_OUTPUT, FINAL_OUTPUT)
