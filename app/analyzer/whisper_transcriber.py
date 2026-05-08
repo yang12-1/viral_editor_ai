bc929ed79730ea579987ed0c4c07c78f3a38f07f
yang12-1 <a01050485358@gmail.com>
2026-05-08 23:20:08 +0900

    Update whisper_transcriber.py

--- app/analyzer/whisper_transcriber.py
+++ app/analyzer/whisper_transcriber.py
@@ -1,7 +1,7 @@
 import whisper
 
 # 'medium' 모델은 정확하지만 무거우므로 성능과 타협 시 'small' 권장
-model = whisper.load_model("medium")
+model = whisper.load_model("small")
 
 def transcribe_video_with_words(video_path):
     # word_timestamps를 True로 설정하여 단어별 시작/종료 시간 획득
